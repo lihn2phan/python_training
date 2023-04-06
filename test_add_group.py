dd0f18c4bc980c187980baf6fd6146e9b4308fe9
lihn2phan <angelina.lihn.brazhnikova@gmail.com>
2023-04-06 12:10:15 +0300

    Добавила класс для группы Изменила параметры методов, используя класс группы

--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
-
+from group import Group
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
@@ -13,7 +13,7 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="dfdfdf", header="dfdfdf", footer="dffdf")
+        self.create_group(wd, Group(name="dfdfdf", header="dfdfdf", footer="dffdf"))
         self.return_to_groups_page(wd)
         self.logout(wd)
     def test_add_empty_group(self):
@@ -21,7 +21,7 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="", header="", footer="")
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
     def logout(self, wd):
@@ -31,19 +31,19 @@ class TestAddGroup(unittest.TestCase):
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("%s" % name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_xpath("//form[@action='/addressbook/group.php']").click()
         # submit group creation
         wd.find_element_by_name("submit").click()
