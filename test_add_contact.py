c16f34a610c57aa4e9ad6152cf558a6c704ba5c2
lihn2phan <angelina.lihn.brazhnikova@gmail.com>
2023-04-09 21:35:13 +0300

    Создала класс Application и перенесла туда все вспомогательные методы из test_add_group и test_add_contact

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,104 +1,43 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
 from contact import Contact
+from application import Application
+import pytest
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    contact1 = Contact(first_name="f_name1",
+                       middle_name="m_name1",
+                       last_name="l_name1",
+                       nickname="nickname1",
+                       photo_path="C:\\fakepath\\1.jpg",
+                       title="title1",
+                       company="company1",
+                       address="address1",
+                       home="home1",
+                       mobile="mobile1",
+                       work="work1",
+                       fax="fax1",
+                       email="emaill1", email2="emaill2", email3="emaill3",
+                       homepage="homepage1",
+                       birthday_day="1",
+                       birthday_month="January",
+                       birthday_year="2000",
+                       anniversary_day="5",
+                       anniversary_month="July",
+                       anniversary_year="2000",
+                       group="group1",
+                       secondary_address="sec_addr1",
+                       secondary_home="sec_home1",
+                       secondary_notes="sec_notes1")
+    app.create_user(contact1)
+    app.logout()
 
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
 
-    def test_add_contact(self):
-        self.login()
-        contact1 = Contact(first_name="f_name1",
-                           middle_name="m_name1",
-                           last_name="l_name1",
-                           nickname="nickname1",
-                           photo_path="C:\\fakepath\\1.jpg",
-                           title="title1",
-                           company="company1",
-                           address="address1",
-                           home="home1",
-                           mobile="mobile1",
-                           work="work1",
-                           fax="fax1",
-                           email="emaill1", email2="emaill2", email3="emaill3",
-                           homepage="homepage1",
-                           birthday_day="1",
-                           birthday_month="January",
-                           birthday_year="2000",
-                           anniversary_day="5",
-                           anniversary_month="July",
-                           anniversary_year="2000",
-                           group="group1",
-                           secondary_address="sec_addr1",
-                           secondary_home="sec_home1",
-                           secondary_notes="sec_notes1")
-        self.create_user(contact1)
-        self.logout()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
-    def fill_field(self, name_element, text):
-        wd = self.wd
-        wd.find_element_by_name(name_element).click()
-        wd.find_element_by_name(name_element).clear()
-        wd.find_element_by_name(name_element).send_keys(text)
 
-    def create_user(self, contact):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-        self.fill_field("firstname", contact.first_name)
-        self.fill_field("middlename", contact.middle_name)
-        self.fill_field("lastname", contact.last_name)
-        self.fill_field("nickname", contact.nickname)
-        self.fill_field("title", contact.title)
-        self.fill_field("company", contact.company)
-        self.fill_field("address", contact.address)
-        self.fill_field("home", contact.home)
-        self.fill_field("mobile", contact.mobile)
-        self.fill_field("work", contact.work)
-        self.fill_field("fax", contact.fax)
-        self.fill_field("email", contact.email)
-        self.fill_field("email2", contact.email2)
-        self.fill_field("email3", contact.email3)
-        self.fill_field("homepage", contact.homepage)
-
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.birthday_day)
-        wd.find_element_by_xpath(f"//option[@value='{contact.birthday_day}']").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.birthday_month)
-        self.fill_field("byear", contact.birthday_year)
-
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.anniversary_day)
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.anniversary_month)
-        self.fill_field("ayear", contact.anniversary_year)
-
-        self.fill_field("address2", contact.secondary_address)
-        self.fill_field("phone2", contact.secondary_home)
-        self.fill_field("notes", contact.secondary_notes)
-        # submit contact creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.open_home_page()
-
-    def login(self):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
--- test_add_group.py
+++ test_add_group.py
@@ -1,71 +1,20 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
 from group import Group
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+from application import Application
+import pytest
 
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="dfdfdf", header="dfdfdf", footer="dffdf"))
-        self.logout()
-    def test_add_empty_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="dfdfdf", header="dfdfdf", footer="dffdf"))
+    app.logout()
 
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        # init group creation
-        self.open_groups_page()
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_xpath("//form[@action='/addressbook/group.php']").click()
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_xpath("//div[@id='footer']/ul/li").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-
-
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
