788a925ebbc96570c9fcac3baf77ee60954be2f7
lihn2phan <angelina.lihn.brazhnikova@gmail.com>
2023-04-14 14:31:16 +0300

    Добавлены тесты для модификации и удаления первой группы и первого контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -39,3 +39,34 @@ class ContactHelper:
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to.alert.accept()
         self.app.open_home_page()
+
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        self.fill_field("firstname", contact.first_name)
+        self.fill_field("middlename", contact.middle_name)
+        self.fill_field("lastname", contact.last_name)
+        self.fill_field("nickname", contact.nickname)
+        self.fill_field("title", contact.title)
+        self.fill_field("company", contact.company)
+        self.fill_field("address", contact.address)
+        self.fill_field("home", contact.home)
+        self.fill_field("mobile", contact.mobile)
+        self.fill_field("work", contact.work)
+        self.fill_field("fax", contact.fax)
+        self.fill_field("email", contact.email)
+        self.fill_field("email2", contact.email2)
+        self.fill_field("email3", contact.email3)
+        self.fill_field("homepage", contact.homepage)
+
+        wd.find_element_by_xpath(f"//option[@value='{contact.birthday_day}']").click()
+        self.fill_field("byear", contact.birthday_year)
+
+        self.fill_field("ayear", contact.anniversary_year)
+
+        self.fill_field("address2", contact.secondary_address)
+        self.fill_field("phone2", contact.secondary_home)
+        self.fill_field("notes", contact.secondary_notes)
+        # submit contact creation
+        wd.find_element_by_name("update").click()
