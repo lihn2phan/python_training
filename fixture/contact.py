0b6e7ff0d8f56d5177ac99ab27c0c773732b18e9
lihn2phan <angelina.lihn.brazhnikova@gmail.com>
2023-04-22 08:37:07 +0300

    Выделила отдельный метод fill_fields чтобы не было дублирования кода

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,9 +7,7 @@ class ContactHelper:
         wd.find_element_by_name(name_element).clear()
         wd.find_element_by_name(name_element).send_keys(text)
 
-    def create(self, contact):
-        wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+    def fill_fields(self, contact):
         self.fill_field("firstname", contact.first_name)
         self.fill_field("middlename", contact.middle_name)
         self.fill_field("lastname", contact.last_name)
@@ -28,7 +26,11 @@ class ContactHelper:
         self.fill_field("address2", contact.secondary_address)
         self.fill_field("phone2", contact.secondary_home)
         self.fill_field("notes", contact.secondary_notes)
-        # submit contact creation
+
+    def create(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        self.fill_fields(contact)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.app.open_home_page()
 
@@ -44,29 +46,12 @@ class ContactHelper:
         wd = self.app.wd
         self.app.open_home_page()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        self.fill_field("firstname", contact.first_name)
-        self.fill_field("middlename", contact.middle_name)
-        self.fill_field("lastname", contact.last_name)
-        self.fill_field("nickname", contact.nickname)
-        self.fill_field("title", contact.title)
-        self.fill_field("company", contact.company)
-        self.fill_field("address", contact.address)
-        self.fill_field("home", contact.home)
-        self.fill_field("mobile", contact.mobile)
-        self.fill_field("work", contact.work)
-        self.fill_field("fax", contact.fax)
-        self.fill_field("email", contact.email)
-        self.fill_field("email2", contact.email2)
-        self.fill_field("email3", contact.email3)
-        self.fill_field("homepage", contact.homepage)
-
-        wd.find_element_by_xpath(f"//option[@value='{contact.birthday_day}']").click()
-        self.fill_field("byear", contact.birthday_year)
-
-        self.fill_field("ayear", contact.anniversary_year)
-
-        self.fill_field("address2", contact.secondary_address)
-        self.fill_field("phone2", contact.secondary_home)
-        self.fill_field("notes", contact.secondary_notes)
-        # submit contact creation
+        self.fill_fields(contact)
         wd.find_element_by_name("update").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        s = int(wd.find_element_by_xpath("//span[@id='search_count']").text)
+        d = 5
+        return int(wd.find_element_by_xpath("//span[@id='search_count']").text)
