0dad585968591d9e0804978063f1e83936ad16f0
lihn2phan <angelina.lihn.brazhnikova@gmail.com>
2023-05-06 19:51:56 +0300

    Параметризовала тесты для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,33 +1,28 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-
-def test_add_contact(app):
-    contact = Contact(first_name="Ivan",
-                       middle_name="m_name1",
-                       last_name="Petrov",
-                       nickname="nickname1",
-                       photo_path="C:\\fakepath\\1.jpg",
-                       title="title1",
-                       company="company1",
-                       address="address1",
-                       homephone="home1",
-                       mobilephone="mobile1",
-                       workphone="work1",
-                       faxphone="fax1",
-                       email="emaill1", email2="emaill2", email3="emaill3",
-                       homepage="homepage1",
-                       birthday_day="1",
-                       birthday_month="January",
-                       birthday_year="2000",
-                       anniversary_day="5",
-                       anniversary_month="July",
-                       anniversary_year="2000",
-                       group="group1",
-                       secondary_address="sec_addr1",
-                       secondaryphone="sec_home1",
-                       secondary_notes="sec_notes1")
+import pytest
+import random
+import string
 
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(first_name="", last_name="", address="",
+                    homephone="", mobilephone="", workphone="",
+                    secondaryphone="", email="", email2="", email3="")] \
+           + [Contact(first_name=random_string("name", 10), last_name=random_string("name", 10),
+                      address=random_string("name", 10), homephone=random_string("name", 11),
+                      mobilephone=random_string("name", 11), workphone=random_string("name", 11),
+                      secondaryphone=random_string("name", 11), email=random_string("name", 10),
+                      email2=random_string("name", 10), email3=random_string("name", 10))
+              for i in range(5)
+              ]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
 
     app.contact.create(contact)
@@ -35,9 +30,3 @@ def test_add_contact(app):
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-
-
-
-
