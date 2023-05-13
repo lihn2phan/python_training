f36d91b137251f289eb02aa86d227d26e0f2edd8
lihn2phan <angelina.lihn.brazhnikova@gmail.com>
2023-05-13 23:43:47 +0300

    Созданы генераторы тестовых данных для групп

--- conftest.py
+++ conftest.py
@@ -2,8 +2,10 @@ from fixture.application import Application
 import pytest
 import json
 import os.path
+import importlib
 fixture = None
 target = None
+import jsonpickle
 @pytest.fixture
 def app(request):
     global fixture
@@ -26,6 +28,21 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
-def pytest_addoption(parser, base_url="http://localhost/addressbook/index.php"):
+def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+def load_from_module(module):
+    return importlib.import_module(f"data.{module}").testdata
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), f"data/{file}.json")) as f:
+        return jsonpickle.decode(f.read())
