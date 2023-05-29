88f06046fa34c9c28ca8d0274de574e64f4a7906
lihn2phan <angelina.lihn.brazhnikova@gmail.com>
2023-05-29 23:12:49 +0300

    Добавила доп проверки

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -8,9 +8,27 @@ def test_add_contact_to_group(app):
     groups = db.get_group_list()
     if len(contacts) == 0:
         app.contact.create(Contact(first_name="contt", middle_name="m_name1"))
+        contacts = db.get_contact_list()
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
-    contact = random.choice(contacts)
+        groups = db.get_group_list()
+
     group = random.choice(groups)
+    contact = random.choice(contacts)
+
+    if len(db.get_contacts_not_in_group(group)) == 0:
+        app.contact.delete_contact_from_group(contact)
+    else:
+        contact = random.choice(db.get_contacts_not_in_group(group))
+
     app.contact.add_contact_to_group(contact.id, group.id)
     assert contact in db.get_contacts_in_group(group)
+
+'''
+1. Если нет групп, создаем группу
+2. Если нет контакта, создаем контакт
+3. Выбираем рандомную группу
+4. Если в группе вообще все контакты, которые есть в системе, то удаляем рандомный контакт (contact) из группы 
+5. Если в группе нет каких-то контактов, то выбираем рандомный контакт (contact) из тех, которых нет в группе
+6. Добавляем выбранный контакт в группу
+'''
--- test/test_del_contact_from_group.py
+++ test/test_del_contact_from_group.py
@@ -1,13 +1,26 @@
+from model.contact import Contact
+from model.group import Group
 import random
 from fixture.orm import ORMFixture
 def test_del_contact_from_group(app):
     db = ORMFixture(host="127.0.0.1", database="addressbook", user="root", password="")
-    contacts = db.get_contact_list()
     groups = db.get_group_list()
-    contact = random.choice(contacts)
+    contacts = db.get_contact_list()
+
+    if len(contacts) == 0:
+        app.contact.create(Contact(first_name="contt", middle_name="m_name1"))
+        contacts = db.get_contact_list()
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name="test"))
+        groups = db.get_group_list()
+
     group = random.choice(groups)
-    if contact in db.get_contacts_not_in_group(group):
+    contact = random.choice(contacts)
+
+    if len(db.get_contacts_in_group(group)) == 0:
         app.contact.add_contact_to_group(contact.id, group.id)
+    else:
+        contact = random.choice(db.get_contacts_in_group(group))
 
     app.contact.delete_contact_from_group(contact.id, group.id)
     assert contact in db.get_contacts_not_in_group(group)
