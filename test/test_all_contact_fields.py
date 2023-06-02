6bcc7af045a7993d8ab85f701202d20a92d7e665
lihn2phan <angelina.lihn.brazhnikova@gmail.com>
2023-06-02 11:29:33 +0300

    Изменила фукции очистки для данных

--- test/test_all_contact_fields.py
+++ test/test_all_contact_fields.py
@@ -8,23 +8,25 @@ def test_all_contact_fields(app, db):
     for i in range(count):
         contact_from_home_page = contacts_from_home_page[i]
         contact_from_db = contacts_from_db[i]
-        assert clear(contact_from_home_page.all_phones_from_home_page) == clear(merge_phones_like_on_home_page(contact_from_db))
-        assert clear(contact_from_home_page.all_email_from_home_page) == clear(merge_email_like_on_home_page(contact_from_db))
-        assert contact_from_home_page.address == contact_from_db.address
-        assert contact_from_home_page.first_name == contact_from_db.first_name
-        assert contact_from_home_page.last_name == contact_from_db.last_name
+        assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db)
+        assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_db)
+        assert clear_repeat_spaces(contact_from_home_page.address) == clear_repeat_spaces(contact_from_db.address)
+        assert contact_from_home_page.first_name == clear_repeat_spaces(contact_from_db.first_name)
+        assert contact_from_home_page.last_name == clear_repeat_spaces(contact_from_db.last_name)
 
 
+def clear(s):
+    return re.sub("[() -]", "", s)
+def clear_repeat_spaces(s):
+    return " ".join(s.split())
 
 def merge_email_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x !="",
+                            map(lambda x: clear_repeat_spaces(x),
                                 filter(lambda x: x is not None,
-                                       [contact.email, contact.email2, contact.email3])))
+                                       [contact.email, contact.email2, contact.email3]))))
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x !="",
                             map(lambda x:clear(x),
                                 filter(lambda x: x is not None,
                                        [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]))))
-
-def clear(s):
-    return re.sub("[() -]", "", s)
