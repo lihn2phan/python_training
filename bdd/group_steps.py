7a5ff500ddc79c4834f320d26130de672c44945d
lihn2phan <angelina.lihn.brazhnikova@gmail.com>
2023-06-04 15:18:10 +0300

    Написан тест для ддобавления группы в стиле bdd

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -2,12 +2,12 @@ from pytest_bdd import given, when, then
 from model.group import Group
 
 
-@given('a group list', target_fixture="a group list")
+@given('a group list', target_fixture="group_list")
 def group_list(db):
     return db.get_group_list()
 
 
-@given('a group with <name>, <header> and <footer>', target_fixture="a group with <name>, <header> and <footer>")
+@given('a group with <name>, <header> and <footer>', target_fixture="new_group")
 def new_group(name, header, footer):
     return Group(name=name, header=header, footer=footer)
 
