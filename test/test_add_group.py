8e2deb9cafa57a61e6cdbbc5ce00ed172a4b10ad
lihn2phan <Fynbakee1@>
2023-06-06 23:53:18 +0300

    Добвлены тесты для добавления и удаления группы с помощью robot framework

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import allure
 
 def test_add_group(app, db, check_ui, json_groups):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with allure.step(f'When I add a group {group} to the list'):
+        app.group.create(group)
+    with allure.step("Then the new group list is equal to the old list with the added group"):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
 
